437361cc0d2f5f74255837664952268a3383a3a0
Frances Zhao <84583731+frances-zhao@users.noreply.github.com>
2021-05-24 11:03:15 -0400

    Add files via upload

---
+++ homework/lesson1/lesson1_4.py
@@ -0,0 +1,14 @@
+#sum, difference, product, and quotient of 2 numbers
+#variable declaration
+number_1 = int()
+number_2 = int()
+
+#input
+number_1 = int(input("Insert your first number: "))
+number_2 = int(input("Insert your second number: "))
+
+#printing result
+print("The sum of the two numbers is", number_1 + number_2,".")
+print("The difference between the two numbers is", number_1 - number_2,".")
+print("The product of the two numbers is", number_1*number_2,".")
+print("The quotient of the two numbers is", number_1/number_2,".")
